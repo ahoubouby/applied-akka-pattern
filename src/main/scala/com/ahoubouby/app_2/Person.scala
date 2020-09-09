72e8daeb836bfa1841c0d2e3bdcbaed1d08aa54e
ahoubouby <ahoubouby@gmail.com>
2020-09-09 14:54:36 +0100

    done from approche2: Encapsulationg state by using STATE containers

---
+++ src/main/scala/com/ahoubouby/app_2/Person.scala
@@ -0,0 +1,18 @@
+package com.ahoubouby.app_2
+
+import akka.actor.Actor
+
+object Person {
+  case class Role(id: Int, roleName: Role)
+  case class PersonalInformation(firstName: Option[String] = None,
+                                 lastName: Option[String] = None,
+                                 email: Option[String] = None,
+                                 phone: Option[String] = None,
+                                 roles: Set[Role] = Set.empty)
+}
+class PerosnActor extends Actor {
+  import Person._
+
+  var personalInformation = PersonalInformation()
+  override def receive: Receive = ???
+}
