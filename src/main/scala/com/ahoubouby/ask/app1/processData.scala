79c7610742bc45afb62ac1fddd82754880c1e197
ahoubouby <ahoubouby@gmail.com>
2020-09-09 20:40:42 +0100

    ask pattern in akka actor best practiceé

---
+++ src/main/scala/com/ahoubouby/ask/app1/processData.scala
@@ -0,0 +1,59 @@
+package com.ahoubouby.ask.app1
+
+import akka.actor.Actor
+import akka.actor.Props
+import scala.concurrent.duration._
+import akka.pattern.ask
+import scala.concurrent.ExecutionContext
+import akka.util.Timeout
+import akka.pattern.{ask, pipe}
+object events {
+  case class ProcessData(data: String)
+  case class DataProcessed(data: String)
+
+  def processDataStage1(data: String): String = s"${data} - stage1"
+  def processDataStage2(data: String): String = s"${data} - stage2"
+  def processDataStage3(data: String): String = s"${data} - stage3"
+}
+
+class Stage1() extends Actor {
+
+  import events._
+
+  import scala.concurrent.ExecutionContext
+  val nextStage = context.actorOf(Props(new Stage2()))
+  implicit val ec: ExecutionContext = context.dispatcher
+  implicit val timeout: Timeout = 5.seconds
+
+  override def receive: Receive = {
+
+    case ProcessData(data) =>
+      val processedData = processDataStage1(data)
+      val future = (nextStage ? ProcessData(processedData))
+      future.pipeTo(sender())
+  }
+}
+
+class Stage2() extends Actor {
+  import akka.util.Timeout
+  import events._
+
+  val nextStage = context.actorOf(Props(new Stage3()))
+  implicit val ec: ExecutionContext = context.dispatcher
+  implicit val timeout: Timeout = 5.seconds
+  override def receive: Receive = {
+    case ProcessData(data) =>
+      val processedData = processDataStage2(data)
+      (nextStage ? ProcessData(processedData)).pipeTo(sender())
+  }
+}
+
+class Stage3() extends Actor {
+  import events._
+
+  override def receive: Receive = {
+    case ProcessData(data) =>
+      val processedData = processDataStage3(data)
+      sender() ! DataProcessed(processedData)
+  }
+}
--- src/main/scala/com/ahoubouby/entryPoint.scala
+++ src/main/scala/com/ahoubouby/entryPoint.scala
@@ -1,11 +1,32 @@
 package com.ahoubouby
 
+import akka.actor.{ActorSystem, Props}
+import com.ahoubouby.ask.app1.{Stage1, events}
+import com.typesafe.config.ConfigFactory
+
+import akka.util.Timeout
+import com.ahoubouby.ask.app1.events.DataProcessed
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 object entryPoint extends App {
-
-  import akka.actor.ActorSystem
-  import com.typesafe.config.ConfigFactory
-
+  import akka.pattern.ask
+  //************ Global definition  ************ //
   val conf = ConfigFactory.load("akka.conf")
   val system = ActorSystem("akka_applied_pattern", conf)
 
+  //************ end of Global definition ************//
+
+  //************ implicit section ************ //
+  implicit val timeout = Timeout(5.seconds)
+  implicit val excecutionContext = system.dispatcher
+  //************ end of implicit section ************//
+  val stage = system.actorOf(Props(new Stage1()))
+  val data: String = "abdelwahed stage processing "
+  val future = (stage ? events.ProcessData(data))
+  val result =
+    Await.result(future, timeout.duration).asInstanceOf[DataProcessed]
+  println(result)
+  system.terminate()
 }
